a23499e2f6d12bc70ff91571dedf42afc1df55e4
andrejsa <andrejsa1975@yahoo.com>
2019-07-09 09:38:51 +0300

    added findByPlace arraylist

--- src/main/java/com/example/demo/repo/MarathonRepo.java
+++ src/main/java/com/example/demo/repo/MarathonRepo.java
@@ -6,6 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.model.Marathon;
 
-public interface MarathonRepo extends CrudRepository<Marathon, Integer>{ArrayList<Marathon>findByName(String Name);
+public interface MarathonRepo extends CrudRepository<Marathon, Integer>{
+	ArrayList<Marathon>findByName(String name);
+	
+	ArrayList<Marathon>findByPlace(String place);
 
 }
