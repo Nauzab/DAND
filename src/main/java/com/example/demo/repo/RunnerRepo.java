6f825c2e4f2ab67ca300c34d788b07ac1e5355f7
Slinkyss <daniel284222@gmail.com>
2019-07-09 10:28:19 +0300

    Updating

--- src/main/java/com/example/demo/repo/RunnerRepo.java
+++ src/main/java/com/example/demo/repo/RunnerRepo.java
@@ -5,7 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.demo.model.Runner;
 
 public interface RunnerRepo extends CrudRepository<Runner, Integer> {
-	
+	Runner findByEmailAndPassword(String email, String password);
+	Runner findByNameAndSurname(String name,String surname);
 	
 
 }
